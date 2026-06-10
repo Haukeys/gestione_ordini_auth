3bd21f002771a8ae291c3ad672c09f0e781c7799
Haukeys <leopazienza76@gmail.com>
2026-06-10 17:02:52 +0200

    Initial commit

---
+++ src/main/java/it/itsacademy/gestione_ordini_auth/GestioneOrdiniAuthApplication.java
@@ -0,0 +1,13 @@
+package it.itsacademy.gestione_ordini_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneOrdiniAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestioneOrdiniAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/itsacademy/gestione_ordini_auth/GestioneOrdiniAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package it.itsacademy.gestione_ordini_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneOrdiniAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
